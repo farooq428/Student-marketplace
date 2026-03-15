c23c68f43677b38da900ac787e89fcb2ad154572
M-Umar-Farooq <shanisipra428@gmail.com>
2026-03-15 05:31:35 -0700

    Update axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://student-marketplace-backend-8lmn.onrender.com",
+  baseURL: "https://student-marketplace-backend-8lmn.onrender.com/api",
 });
 
 // Add auth token if exists
